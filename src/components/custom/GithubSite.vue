19e02bb3d848baf0f8b9fceaa60c0477f49bd2b8
docker-xing <150640021+docker-xing@users.noreply.github.com>
2024-05-05 18:22:44 +0800

    Update GithubSite.vue

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="text-neutral-400">
     <span>Star on</span>
-    <a href="https://github.com/Chanzhaoyu/chatgpt-bot" target="_blank" class="text-blue-500">
-      GitHub
+    <a href="https://my.gpt-pro.xyz" target="_blank" class="text-blue-500">
+      ChatGPT3.5
     </a>
   </div>
 </template>
